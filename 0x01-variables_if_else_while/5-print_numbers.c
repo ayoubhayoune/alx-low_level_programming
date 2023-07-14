5bdac87238a28ee53374b2758df1488af3cc00fa
ayoub <ayoub.hayoune1@gmail.com>
2023-07-14 10:38:18 -0700

    Numbers, done!

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints all single-digit numbers of base 10.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int number = 0;
+
+	while (number <= 9)
+	{
+		putchar(number + '0');
+		number++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
+
