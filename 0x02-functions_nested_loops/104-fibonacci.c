7787aa32541fd60ddf064dfe55bd55f0c7e18881
ayoub <ayoub.hayoune1@gmail.com>
2023-07-18 15:11:48 -0700

    fibonacci please this time git add .

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,44 +1,22 @@
 #include <stdio.h>
 
-#define FIBO_SIZE 100
-
 /**
  * print_fibonacci - Prints the first 98 Fibonacci numbers
  */
 void print_fibonacci(void)
 {
-	unsigned int fibo[FIBO_SIZE];
-	unsigned int carry, i, j;
+	unsigned int i, count;
+	unsigned int a = 1, b = 2, next;
 
-	/* Initialize array with zeros */
-	for (i = 0; i < FIBO_SIZE; i++)
-		fibo[i] = 0;
+	printf("%u, %u", a, b);
 
-	fibo[FIBO_SIZE - 1] = 1; /* First Fibonacci number */
-	carry = 0;
-
-	for (i = 2; i <= 98; i++)
+	for (count = 2; count < 98; count++)
 	{
-		for (j = FIBO_SIZE - 1; j > 0; j--)
-		{
-			fibo[j] = fibo[j] + fibo[j - 1] + carry;
-			carry = fibo[j] / 10;
-			fibo[j] %= 10;
-		}
+		next = a + b;
+		printf(", %u", next);
+		a = b;
+		b = next;
 	}
-
-	/* Find the first non-zero digit */
-	for (i = 0; i < FIBO_SIZE; i++)
-	{
-		if (fibo[i] != 0)
-			break;
-	}
-
-	/* Print the Fibonacci numbers */
-	printf("%d", fibo[i]);
-
-	for (j = i + 1; j < FIBO_SIZE; j++)
-		printf(", %d", fibo[j]);
 
 	printf("\n");
 }
