9014c35fe3fcb211d405975353104c5b3eb4576a
ayoub <ayoub.hayoune1@gmail.com>
2023-07-18 15:14:43 -0700

    fibonacci please this time git add .

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,33 +1,32 @@
 #include <stdio.h>
 
 /**
- * print_fibonacci - Prints the first 98 Fibonacci numbers
- */
-void print_fibonacci(void)
-{
-	unsigned int i, count;
-	unsigned int a = 1, b = 2, next;
-
-	printf("%u, %u", a, b);
-
-	for (count = 2; count < 98; count++)
-	{
-		next = a + b;
-		printf(", %u", next);
-		a = b;
-		b = next;
-	}
-
-	printf("\n");
-}
-
-/**
  * main - Entry point of the program
  *
  * Return: Always 0
  */
 int main(void)
 {
-	print_fibonacci();
+	unsigned long int a = 1, b = 2, next, sum = 0;
+	int count;
+
+	printf("%lu, %lu", a, b);
+
+	for (count = 3; count <= 98; count++)
+	{
+		next = a + b;
+		if (next > 4000000)
+			break;
+
+		if (next % 2 == 0)
+			sum += next;
+
+		printf(", %lu", next);
+		a = b;
+		b = next;
+	}
+
+	printf("\nSum of even-valued terms: %lu\n", sum);
+
 	return (0);
 }
