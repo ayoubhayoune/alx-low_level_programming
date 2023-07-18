1fb79bca93ac1cdc53e731c4f832f4d6f4dca5aa
ayoub <ayoub.hayoune1@gmail.com>
2023-07-18 15:08:09 -0700

    fibonacci please this time !

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,5 +1,48 @@
 #include <stdio.h>
 
+#define FIBO_SIZE 100
+
+/**
+ * print_fibonacci - Prints the first 98 Fibonacci numbers
+ */
+void print_fibonacci(void)
+{
+	unsigned int fibo[FIBO_SIZE];
+	unsigned int carry, i, j;
+
+	/* Initialize array with zeros */
+	for (i = 0; i < FIBO_SIZE; i++)
+		fibo[i] = 0;
+
+	fibo[FIBO_SIZE - 1] = 1; /* First Fibonacci number */
+	carry = 0;
+
+	for (i = 2; i <= 98; i++)
+	{
+		for (j = FIBO_SIZE - 1; j > 0; j--)
+		{
+			fibo[j] = fibo[j] + fibo[j - 1] + carry;
+			carry = fibo[j] / 10;
+			fibo[j] %= 10;
+		}
+	}
+
+	/* Find the first non-zero digit */
+	for (i = 0; i < FIBO_SIZE; i++)
+	{
+		if (fibo[i] != 0)
+			break;
+	}
+
+	/* Print the Fibonacci numbers */
+	printf("%d", fibo[i]);
+
+	for (j = i + 1; j < FIBO_SIZE; j++)
+		printf(", %d", fibo[j]);
+
+	printf("\n");
+}
+
 /**
  * main - Entry point of the program
  *
@@ -7,20 +50,6 @@
  */
 int main(void)
 {
-	unsigned long int num1 = 1, num2 = 2, next;
-	int count;
-
-	printf("%lu, %lu", num1, num2);
-
-	for (count = 3; count <= 98; count++)
-	{
-		next = num1 + num2;
-		printf(", %lu", next);
-		num1 = num2;
-		num2 = next;
-	}
-
-	printf("\n");
-
+	print_fibonacci();
 	return (0);
 }
