b25cdd21046e0f5920a03961a45c0ca520284380
ayoub <ayoub.hayoune1@gmail.com>
2023-07-18 15:48:44 -0700

    fibonacci please

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,28 +1,65 @@
+#include "main.h"
+
+/**
+ * numLength - returns the length of a number
+ * @num: operand number
+ *
+ * Return: number of digits
+ */
+int numLength(int num)
+{
+	int length = 0;
+
+	if (!num)
+		return (1);
+
+	while (num)
+	{
+		num = num / 10;
+		length += 1;
+	}
+
+	return (length);
+}
 
 /**
  * main - Entry point
  *
- * Description: Prints the first 98 Fibonacci numbers, starting with 1 and 2
+ * Description: Prints the first 98 Fibonacci numbers starting with 1 and 2,
+ *              followed by a new line.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	unsigned long long int fib1 = 1, fib2 = 2, fib_sum;
-	int count;
+	int count, initial0s;
+	unsigned long f1 = 1, f2 = 2, sum, mx = 100000000, flo = 0, f20 = 0, sumo = 0;
 
-	printf("%llu, %llu", fib1, fib2);
-
-	for (count = 3; count <= 98; count++)
+	for (count = 1; count <= 98; count++)
 	{
-		fib_sum = fib1 + fib2;
-		printf(", %llu", fib_sum);
+		if (flo > 0)
+			printf("%lu", flo);
 
-		fib1 = fib2;
-		fib2 = fib_sum;
+		initial0s = numLength(mx) - 1 - numLength(f1);
+		while (flo > 0 && initial0s > 0)
+		{
+			printf("%d", 0);
+			initial0s--;
+		}
+
+		printf("%lu", f1);
+		sum = (f1 * f2) % mx;
+		sumo = flo + f20 + (f1 + f2) / mx;
+		f1 = f2;
+		flo = f20;
+		f2 = sum;
+		f20 = sumo;
+
+		if (count != 98)
+			printf(", ");
+		else
+			printf("\n");
 	}
-
-	printf("\n");
 
 	return (0);
 }
