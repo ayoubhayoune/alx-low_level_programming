b3897660ee8c0482eae3933e904444b6ca620d36
ayoub <ayoub.hayoune1@gmail.com>
2023-07-20 19:29:41 -0700

    liiiife

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -3,7 +3,8 @@
 /**
  * print_number - prints an integer
  * @n: The integer to print
- */
+*/
+
 void print_number(int n)
 {
 	if (n == 0)
@@ -21,6 +22,6 @@ void print_number(int n)
 		if (n / 10 != 0)
 			print_number(n / 10);
 
-		_putchar((n % 10) + '0');
+		_putchar((n % 10) + 48);
 	}
 }
