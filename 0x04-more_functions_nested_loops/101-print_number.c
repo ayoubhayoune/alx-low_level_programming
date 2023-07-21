e5a9a3ac3dd0c8262d4bfcdffffb15614c18d7ef
ayoub <ayoub.hayoune1@gmail.com>
2023-07-20 19:20:11 -0700

    life

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -4,18 +4,23 @@
  * print_number - prints an integer
  *
  * @n: The integer to print
-*/
+ */
 void print_number(int n)
 {
-	unsigned int num = n;
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
 
-	/*first check if its negative*/
 	if (n < 0)
 	{
 		_putchar('-');
-		num = num;
+		n = -n;
 	}
 
-	/*print the first few digits*/
-	_putchar((num % 10) + 48);
+	if (n / 10 != 0)
+		print_number(n / 10);
+
+	_putchar((n % 10) + '0');
 }
