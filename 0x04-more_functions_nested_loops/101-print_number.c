4499d6815df5078bcb44d2b5a162c14d21f114fb
ayoub <ayoub.hayoune1@gmail.com>
2023-07-20 19:23:47 -0700

    life !

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -2,7 +2,6 @@
 
 /**
  * print_number - prints an integer
- *
  * @n: The integer to print
  */
 void print_number(int n)
@@ -10,17 +9,18 @@ void print_number(int n)
 	if (n == 0)
 	{
 		_putchar('0');
-		return;
 	}
-
-	if (n < 0)
+	else
 	{
-		_putchar('-');
-		n = -n;
+		if (n < 0)
+		{
+			_putchar('-');
+			n = -n;
+		}
+
+		if (n / 10 != 0)
+			print_number(n / 10);
+
+		_putchar((n % 10) + '0');
 	}
-
-	if (n / 10 != 0)
-		print_number(n / 10);
-
-	_putchar((n % 10) + '0');
 }
