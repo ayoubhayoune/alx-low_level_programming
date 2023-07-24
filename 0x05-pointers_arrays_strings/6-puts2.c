7d95a0e050546b3ea303a8897d5a10a5f39afecc
ayoub <ayoub.hayoune1@gmail.com>
2023-07-24 14:21:14 -0700

    y

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -4,14 +4,16 @@
  * puts2 - Prints every other character of a string.
  * @str: Pointer to the string.
  */
+
 void puts2(char *str)
 {
 	int i = 0;
 
-	while (str[i] != '\0')
+	while (str[i] != '\0' && str[i + 1] != '\0')
 	{
 		_putchar(str[i]);
 		i += 2;
 	}
+
 	_putchar('\n');
 }
