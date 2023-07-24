879b01f690d33d30bb1d5f7e29323a59b6c9f7c8
ayoub <ayoub.hayoune1@gmail.com>
2023-07-24 14:58:22 -0700

    f

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -13,10 +13,10 @@ int _atoi(char *s)
 	int sign = 1;
 
 	do {
-		if ($s == '-')
+		if (*s == '-')
 			sign *= -1;
-		else if ($s >= '0' && $s <= '9')
-			num = (num * 10) + ($s - '0');
+		else if (*s >= '0' && *s <= '9')
+			num = (num * 10) + (*s - '0');
 		else if (num > 0)
 			break;
 	} while (*s++);
