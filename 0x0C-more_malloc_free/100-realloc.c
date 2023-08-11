8af114c6e73f6c85822f4d196da07d08b6bc5eb8
ayoub <ayoub.hayoune1@gmail.com>
2023-08-10 18:41:19 -0700

    tsk4

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -14,16 +14,16 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	void *new_ptr;
 
-	if (ptr == NULL)
-	return (malloc(new_size));
-
 	if (new_size == 0)
 	{
 	free(ptr);
 	return (NULL);
 	}
 
-	if (new_size == old_size)
+	if (ptr == NULL)
+	return (malloc(new_size));
+
+	if (new_size <= old_size)
 	return (ptr);
 
 	new_ptr = malloc(new_size);
@@ -31,11 +31,8 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (new_ptr == NULL)
 	return (NULL);
 
-	if (new_size > old_size)
-	old_size = new_size;
-
 	for (unsigned int i = 0; i < old_size; i++)
-	((char *)new_ptr)[i] = ((char *)ptr)[i];
+	*((char *)new_ptr + i) = *((char *)ptr + i);
 
 	free(ptr);
 	return (new_ptr);
