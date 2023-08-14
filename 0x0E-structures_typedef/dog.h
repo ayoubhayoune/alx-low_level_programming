da4ebbf5774702ceefa61cb996fd6f099f63ac63
ayoub <ayoub.hayoune1@gmail.com>
2023-08-14 14:52:04 -0700

    tsk0, 0x0E. C - Structures, typedef

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Represents a dog's attributes.
+ * @name: Pointer to dog's name.
+ * @age: Age of the dog.
+ * @owner: Pointer to owner's name.
+ *
+ * Description: dog struct in a big word
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
