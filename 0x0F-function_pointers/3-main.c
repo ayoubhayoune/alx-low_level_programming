ac16b93ce02e6999bcd0f69cb298066a32aa51bf
ayoub <ayoub.hayoune1@gmail.com>
2023-08-16 16:16:41 -0700

    tsk3

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -11,34 +11,21 @@
  */
 int main(int argc, char *argv[])
 {
-	int num1, num2, result;
-	int (*op_func)(int, int);
+	int (*op_func)(int, int), a, b;
 
 	if (argc != 4)
-	{
-		printf("Error\n");
-		return (98);
-	}
+		printf("Error\n"), exit(98);
 
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[3]);
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
 
 	op_func = get_op_func(argv[2]);
-
 	if (!op_func)
-	{
-		printf("Error\n");
-		return (99);
-	}
+		printf("Error\n"), exit(99);
 
-	if ((*argv[2] == '/' || *argv[2] == '%') && num2 == 0)
-	{
-		printf("Error\n");
-		return (100);
-	}
+	if (!b && (argv[2][0] == '/' || *argv[2][0] == '%'))
+		printf("Error\n"), exit(100);
 
-	result = op_func(num1, num2);
-	printf("%d\n", result);
-
+	printf("%d\n", op_func(a, b));
 	return (0);
 }
