84c169211afe438f101f097adc03dedaa81e5d26
ayoub <ayoub.hayoune1@gmail.com>
2023-08-16 16:19:01 -0700

    tsk3

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,5 +1,5 @@
-#ifndef _3_CALC_H
-#define _3_CALC_H
+#ifndef _CALC_H
+#define _CALC_H
 
 #include <stdlib>
 #include <stdio>
@@ -22,4 +22,6 @@ int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
 
-#endif /* _3_CALC_H */
+int (*get_op_func(char *s))(int, int);
+
+#endif
