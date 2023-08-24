ec28714d1d296d616d518cb9bbc1d7afc01bc3f6
ayoub <ayoub.hayoune1@gmail.com>
2023-08-24 15:06:30 -0700

    tsk6

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,14 +1,12 @@
-section .data
-    msg db 'Hello, Holberton', 0xa, 0
-
-section .text
-    extern printf
-    global main
+section		.text
+	extern	printf
+	global	main
 
 main:
-    mov     edi, msg
-    mov     eax, 0
-    call    printf
+	mov		edi, msg
+	mov		eax, 0
+	call	printf
 
-    xor     edi, edi
-    call    exit
+
+section		.data
+	msg db 'Hello, Holberton', 0xa, 0
