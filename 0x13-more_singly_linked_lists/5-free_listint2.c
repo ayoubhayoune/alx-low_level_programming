fdba02dd7aa79bcaf28352482004d63982aea265
ayoub <ayoub.hayoune1@gmail.com>
2023-08-28 13:17:59 -0700

    tsk5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -3,7 +3,7 @@
 /**
  * free_listint2 - Frees a listint_t list.
  * @head: Pointer to the pointer of the head of the list.
- * 
+ *
  * The function sets the head to NULL.
  */
 void free_listint2(listint_t **head)
