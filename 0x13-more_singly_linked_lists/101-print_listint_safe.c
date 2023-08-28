cd5b60ed5b270ca54f83303dbbc934714f5097c4
ayoub <ayoub.hayoune1@gmail.com>
2023-08-28 13:49:36 -0700

    tsk101

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -3,33 +3,64 @@
 #include <stdlib.h>
 
 /**
- * print_listint_safe - Prints a listint_t linked list.
- * @head: Pointer to the head of the list.
+ * _realloc_list - Reallocates memory for an array of pointers to nodes.
+ * @old_list: Pointer to the old list.
+ * @size: Size of the new list.
+ * @new_node: Pointer to the new node to add.
+ *
+ * Return: Pointer to the new list.
+ */
+const listint_t **_realloc_list(const listint_t **old_list,
+					size_t size, const listint_t *new_node)
+{
+	const listint_t **new_list;
+	size_t i;
+
+	new_list = malloc(size * sizeof(listint_t *));
+	if (new_list == NULL)
+	{
+		free(old_list);
+		exit(98);
+	}
+
+	for (i = 0; i < size - 1; i++)
+		new_list[i] = old_list[i];
+
+	new_list[i] = new_node;
+	free(old_list);
+
+	return (new_list);
+}
+
+/**
+ * print_listint_safe - Prints a listint_t linked list safely.
+ * @head: Pointer to the start of the list.
  *
  * Return: The number of nodes in the list.
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	size_t node_count = 0;
-	const listint_t *current = head;
-	const listint_t *loop_start = NULL;
+	size_t num = 0;
+	const listint_t **node_list = NULL;
 
-	while (current)
+	while (head != NULL)
 	{
-		printf("[%p] %d\n", (void *)current, current->n);
-		node_count++;
+		for (size_t i = 0; i < num; i++)
+		{
+			if (head == node_list[i])
+			{
+				printf("-> [%p] %d\n", (void *)head, head->n);
+				free(node_list);
+				return (num);
+			}
+		}
 
-		if (current > loop_start)
-		{
-			loop_start = current;
-			current = current->next;
-		}
-		else
-		{
-			printf("-> [%p] %d\n", (void *)current->next, current->next->n);
-			break;
-		}
+		num++;
+		node_list = _realloc_list(node_list, num, head);
+		printf("[%p] %d\n", (void *)head, head->n);
+		head = head->next;
 	}
 
-	return (node_count);
+	free(node_list);
+	return (num);
 }
